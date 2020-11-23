19f335751bb43e92c7266415b170d8bba6d06424
Natal'ya Pankina <swo.mdm@gmail.com>
2020-11-23 17:48:52 +0400

    first commit

---
+++ 002.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "    (*)" << endl;
+    cout << " (*) O (*)" << endl;
+    cout << "(*) (*) (*)" << endl;
+    cout << " \\ XXXXX /" << endl;
+    cout << "  \\ XXX /" << endl;
+    cout << "   \\ X /" << endl;
+    cout << "     V" << endl;
+    return 0;
+}
+
+
