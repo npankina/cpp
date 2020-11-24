e584845a6982be8a8d861000f20dfa9af97eaaa9
Natal'ya Pankina <swo.mdm@gmail.com>
2020-11-24 22:41:56 +0400

    add 2 progs

---
+++ 004.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    string name;
+
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << "Hello, " << name << endl;
+    return 0;
+}
